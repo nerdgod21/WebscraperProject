30cd6886a3e873c84b2fe1e899c9fc1481b643ea
nerdgod21 <56525761+nerdgod21@users.noreply.github.com>
2022-04-12 22:51:11 -0700

    Update Project2.py

--- Project2.py
+++ Project2.py
@@ -2,19 +2,23 @@ from ast import Import
 from cgi import parse
 import scrapy
 from scrapy import cmdline
-
-
+import webbrowser
+import os
 class BlogSpider(scrapy.Spider):
+  #Set Scrapy to take the price websites
     w1 = 'KAMINEOSET'
     w2 = 'REVISESET'
-    w3 = 'Mind'
-    w4 = 'Sculptor'
-    t = '#online'
+    #Different sets
     name = 'price'
     start_urls = ['https://tinyurl.com/{}'.format(w1)]
     def parse(self, response):
       page = response.url
-      filename = 'Prices.html'
-      with open (filename, 'wb') as f :
-            f.write (response.body)
-cmdline.execute("scrapy runspider Project2.py".split())
+      filenam = 'Prices.html'
+      with open(filenam, 'wb') as f:
+            f.write(response.body)
+      #create HTML file out of website
+filename = 'file:///'+os.getcwd()+'/' + 'Prices.html'
+webbrowser.open_new_tab(filename)
+#Open the price file
+cmdline.execute("scrapy runspider WebProject/Project2.py".split())
+#Necesary scrapy run
