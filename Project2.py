60321f1d6799712e5b572d514a35b7d81375db1a
nerdgod21 <56525761+nerdgod21@users.noreply.github.com>
2022-04-13 21:05:52 -0700

    Update Project2.py

--- Project2.py
+++ Project2.py
@@ -4,19 +4,26 @@ import scrapy
 from scrapy import cmdline
 import webbrowser
 import os
+from lxml import html
+import requests
+page = requests.get('https://www.ebay.com/itm/403592776720?hash=item5df8010c10:g:jncAAOSwsptiPwy~.html')
+tree = html.fromstring(page.content)
+buyers = tree.xpath('//*[@id="viTabs_0_is"]/div/div[2]/div/div[4]/div[4]/div/div/span/text()')
+
 class BlogSpider(scrapy.Spider):
   #Set Scrapy to take the price websites
-    w1 = 'KAMINEOSET'
-    w2 = 'REVISESET'
     #Different sets
-    name = 'price'
-    start_urls = ['https://tinyurl.com/{}'.format(w1)]
+    if buyers == ['Kamigawa: Neon Dynasty']:
+        setname = "KAMINEOSET"
+    name = 'Price'
+    start_urls = ['https://tinyurl.com/{}'.format(setname)]
     def parse(self, response):
       page = response.url
       filenam = 'Prices.html'
       with open(filenam, 'wb') as f:
             f.write(response.body)
       #create HTML file out of website
+
 filename = 'file:///'+os.getcwd()+'/' + 'Prices.html'
 webbrowser.open_new_tab(filename)
 #Open the price file
